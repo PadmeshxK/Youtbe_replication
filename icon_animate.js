5f9878c88c6bdc7d02945dc50d557b4f612354a3
PadmeshxK <hihello16112004@gmail.com>
2023-03-15 17:25:13 +0530

    update 4.2
    bug fixes

--- icon_animate.js
+++ icon_animate.js
@@ -1,14 +1,22 @@
-const icons_element = document.querySelectorAll('.icons_container, .mic_button, .left_icon_container')
+const icons_element = document.querySelectorAll('.icons_container, .mic_button, .left_icon_container, .search_button_media_660')
+const video_border_animate_element  = document.querySelectorAll('.video_container')
 let animation_duration = 800
 
 icons_element.forEach((element) => {
     element.addEventListener('mouseup',() =>{
-        icon_border_animate(element)
+        icon_border_animate(element,'outering_icon')
     })
 })
 
-function icon_border_animate(element){
-    element.style.animationName = 'outering'
+video_border_animate_element.forEach((element) => {
+    element.addEventListener('mouseup',() => {
+        icon_border_animate(element,'videoring')
+    })
+})
+
+function icon_border_animate(element,name){
+    console.log(element)
+    element.style.animationName = name
     element.style.animationDuration = animation_duration + 'ms'
     element.style.animationTimingFunction = 'cubic-bezier(0, 0, 0.2, 1)'
     setTimeout(() =>{
